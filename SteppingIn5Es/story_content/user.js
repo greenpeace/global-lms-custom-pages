cd61bb39ee6f9a732db0fd19101ca97cab9c28fa
emusar <52461177+emusar@users.noreply.github.com>
2021-06-18 14:23:56 +0200

    push new reset 5E's SI

---
+++ SteppingIn5Es/story_content/user.js
@@ -0,0 +1,28 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "65zXvQ4yryT":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var printButton = document.createElement('input');
+printButton.setAttribute('type', 'button');
+
+printButton.setAttribute('id', 'CustomPrint');
+
+printButton.setAttribute('value', 'Print Page');
+
+printButton.setAttribute('onClick', 'window.print()');
+
+document.body.insertBefore(printButton, document.body.firstChild);
+
+$(".volume").after($("#CustomPrint"));
+$("#CustomPrint").addClass('btn cs-button')
+$(".volume").hide();
+}
+
